cac3be12a88a9c7ebed48c0ef50c0e2362e39327
ksee1230 <ksee1230@gmail.com>
2025-05-21 18:41:33 +0900

    feat (HG-110): user, product endpoint log 생성 로직 추가
    - 회원가입, 로그인, 유저 정보 수정, 상품 검색, 상품 상세 페이지 조회에 log 생성
    - logging으로, 일단 계속 특정 파일에 쌓이는 형식
      - log rotate는 일단 `서우님`,`장원님`이 작업하신 스크립트 기반으로 수행 예정
    - user 정보 수정에서, 기존에 언급된 내용으로 맞게 기능 수정

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb://postgres:han00719@mongodb_cart:27017"
+MONGO_URI = "mongodb://postgres:han00719()@mongodb_cart:27017"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client['cart']
 
